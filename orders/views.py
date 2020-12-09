9c70d166bb293f29b444d05be4f0bb301045d93c
CindyKatoni <katmuema@gmail.com>
2020-12-09 16:34:14 +0300

    Created models, view, url for orders

--- orders/views.py
+++ orders/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "item_list.html", context)
